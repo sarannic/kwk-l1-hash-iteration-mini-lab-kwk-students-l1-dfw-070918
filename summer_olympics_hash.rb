9cb07ce8d4ec4a3dcbfc14a9edb0f14879bd0f9e
sarannic <snickershelton11@gmail.com>
2018-07-11 19:08:32 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,6 +1,6 @@
 
 def create_olympics_hash
- summer_olympics = {:Sydeny => 2000, :Athens => 2004, :Beijing => 2008, :London => 2012}
+ summer_olympics = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2012}
  return summer_olympics
 end
 
