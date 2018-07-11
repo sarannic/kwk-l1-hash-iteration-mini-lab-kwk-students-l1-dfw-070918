802135012285a674d3fa3908a645d9559645d34e
sarannic <snickershelton11@gmail.com>
2018-07-11 19:11:08 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -5,7 +5,10 @@ def create_olympics_hash
 end
 
 def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  new_city = :Atlanta
+  new_date = "1996"
+  summer_olympics[new_city] = new_date
+  return summer_olympics
 end
 
 def iterate_through_hash
