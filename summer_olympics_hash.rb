df2b99042df4b942483adb8c5a65ffa30fd90c9a
sarannic <snickershelton11@gmail.com>
2018-07-11 19:35:57 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -13,10 +13,9 @@ def add_a_key_value_pair
 end
 
 def iterate_through_hash
-  add_a_key_value_pair.each do {|city, date|
+  add_a_key_value_pair.each do |city, date|
     puts "The #{city} summer olympics took place in #{date}." 
     return add_a_key_value_pair
-  }
   end
 end
 
